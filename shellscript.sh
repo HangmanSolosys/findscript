db91e2197608024eae4847f20a22a6839fdfd304
HangmanSolosys <ivanignatov9624@gmail.com>
2023-03-10 23:36:37 +0200

    Adding the script to the github repository

---
+++ shellscript.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+prompt_user() {
+	
+	read -p "Hello, please provide me with a filename to search: " check
+
+
+
+echo
+
+echo "looking for $check"
+
+
+
+echo
+
+where=$(find / -name $check) &>/dev/null
+
+
+
+echo
+
+if [ $? -eq 0 ]; then
+
+	echo "we found the $check file in $where"
+
+else
+	echo "Error, file not found"
+
+fi
+
+echo 
+
+}
+
+prompt_user
+
+while [ true ] 
+
+do
+
+prompt_user
+
+done
